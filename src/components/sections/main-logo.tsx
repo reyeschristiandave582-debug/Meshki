36d32908b0a99a1c2051a14e84ee929f6050a9d5
reyeschristiandave582-debug <reyeschristiandave582@gmail.com>
2026-08-05 16:12:19 +0800

    Update main-logo.tsx

--- src/components/sections/main-logo.tsx
+++ src/components/sections/main-logo.tsx
@@ -9,7 +9,7 @@ const MainLogo = () => {
       onClick={() => window.parent.postMessage({ type: "OPEN_EXTERNAL_URL", data: { url: "https://giftclick.org/aff_c?offer_id=302&aff_id=183864&source=Meshki" } }, "*")}
     >
       <img 
-        src="https://i.imgur.com/lfmzARL.png" 
+        src="https://i.imgur.com/dv3RFXp.png" 
         alt="Meshki Logo" 
         className="h-10 sm:h-15 w-20 object-contain transition-all duration-700 hover:brightness-120"
       />
